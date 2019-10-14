80b69c0b6efd132dd0e470a5f4a73e90d5d4ba10
Artem Kazaryan <kazarynarm@mail.ru>
2019-10-14 19:48:38 +0300

    feat: solve task (not fully)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  let balance = [];
+  let right = [];
+  let left = [];
+
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    balance.push(0);
+    right.push(bracketsConfig[i][1]);
+    left.push(bracketsConfig[i][0]);
+  }
+
+  for (let i = 0; i < str.length; i++) {
+    let index = getPosition(str[i], bracketsConfig);
+    if (str[i] === bracketsConfig[index][0]) balance[index]++;
+
+    if (str[i] === bracketsConfig[index][1]) balance[index]--;
+
+    if (balance[index] < 0) return false;
+  }
+  return true;
 }
+
+function getPosition(bracket, bracketsConfig) {
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    if (bracketsConfig[i].indexOf(bracket) !== -1)
+      return i;
+  }
+  return -1;
+}
+
+
